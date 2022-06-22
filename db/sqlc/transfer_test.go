b63080ae1a32ca8d6291ffc5eb58b28020abe313
Hirohisa Kitsuka <hi.kitsuka@gmail.com>
2022-06-22 23:44:17 +0900

    ✅ Add Tst

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"gin_basics/util"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T) Transfer {
+	account1 := CreateRandomAccount(t)
+	account2 := CreateRandomAccount(t)
+
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomMoney(),
+	}
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	createRandomTransfer(t)
+}
+
+func TestGetTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+
+	transfer2, err := testQueries.GetEntry(context.Background(), transfer1.ID)
+
+}
