f0514e1d8fc0f4ffc0895422971d87ca91511272
Hirohisa Kitsuka <hi.kitsuka@gmail.com>
2022-07-03 16:52:45 +0900

    🐍 User Viper and load config

--- main.go
+++ main.go
@@ -4,19 +4,25 @@ import (
 	"database/sql"
 	"gin_basics/api"
 	db "gin_basics/db/sqlc"
+	"gin_basics/util"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5434/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:secret@localhost:5434/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +30,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
