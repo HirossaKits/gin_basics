0c50c6ebaf49baac41e1a5364e0f973a235b4024
Hirohisa Kitsuka <hi.kitsuka@gmail.com>
2022-07-18 23:59:08 +0900

    😊 Add validator

--- api/server.go
+++ api/server.go
@@ -4,6 +4,8 @@ import (
 	db "gin_basics/db/sqlc"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -15,6 +17,10 @@ type Server struct {
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
